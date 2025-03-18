37b3cffed1fe84e08c7efe85fcb088f2a06732ec
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-03-17 23:00:00 -0300

    Add examples of data types with validations using Zod

--- src/zod2.ts
+++ src/zod2.ts
@@ -27,3 +27,17 @@ const exemplo3 = z.object({
     name: z.literal('Clarice'),
     age: z.literal(25),
 });
+
+// Alguns exemplos de tipos de dados com validações que podem ser usados
+// com o Zod.
+const exemplo4 = z.object({
+    name: z.string().min(3).max(50),
+    age: z.number().int().positive().min(18).max(100),
+    url: z.string().url(),
+    email: z.string().email(),
+    emoji: z.string().emoji(),
+    uuid: z.string().uuid(),
+    ip: z.string().ip(),
+    imagem: z.string().endsWith('.jpg'),
+    regra: z.string().regex(/[a-z]{2}/g)
+});
